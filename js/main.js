5dcb16151f1b6f4b2fdd41aa7f3020ea6ef63413
Carlos Munoz Diaz <carlos.munoz@cchs.csic.es>
2022-05-03 16:10:09 +0200

    Cambios en tooltip

--- js/main.js
+++ js/main.js
@@ -12,14 +12,12 @@ d3.queue()
     .await(function(error, mapa, data) {
         if(error) throw error; 
 
-        console.log(data);
-
         //Mapa
         let worldGeojson = topojson.feature(mapa, mapa.objects.ne_110m_admin_0_countries_lakes);
         let worldGeojsonInner = topojson.mesh(mapa, mapa.objects.ne_110m_admin_0_countries_lakes, (a, b) => a !== b);
 
         const features = worldGeojson.features.filter(d => d.properties.CONTINENT.includes("America") && !["Canada", "Greenland", "United States of America"].includes(d.properties.ADMIN));
-
+        
         let laGeojson = {type: 'FeatureCollection', features};
 
         let projection = d3.geoAzimuthalEqualArea()
@@ -27,6 +25,32 @@ d3.queue()
             .fitSize([width - 1, height], laGeojson);
 
         let path = d3.geoPath(projection);
+
+        //Datos
+        let features1 = [], features2 = [];
+        for(let i = 0; i < laGeojson.features.length; i++) {
+            let hasData = data.filter(function(item) {
+                if(item.Pais == laGeojson.features[i].properties.ADMIN) {
+                    return item;
+                }
+            });
+
+            laGeojson.features[i].data = hasData;
+        }
+
+        //Podemos hacer división entre los que sí tienen datos y los que no
+        for(let i = 0; i < laGeojson.features.length; i++) {            
+            if(laGeojson.features[i].data.length != 0) {
+                features1.push(laGeojson.features[i]);
+            } else {
+                features2.push(laGeojson.features[i]);
+            }
+        }
+
+        let laWith = {type: 'FeatureCollection', features: features1};
+        let laWithout = {type: 'FeatureCollection', features: features2};
+
+        console.log(laGeojson, laWith, laWithout);
 
         const svg = d3.select('#map')
             .append('svg')
@@ -38,27 +62,48 @@ d3.queue()
             .attr("d", path)
             .attr("fill", "none")
             .attr("stroke", "#f0f0f0");
-        
-        svg.selectAll(".country")
-            .data(laGeojson.features)
-            .enter().append("path")
-            .attr("class", "country")
-            .attr("fill", "#ddd")
+
+        //Países con datos
+        svg.selectAll("country")
+            .data(laWith.features)
+            .enter()
+            .append("path")
+            .attr("class", "country-yes")
+            .attr("fill", '#113678')
             .attr("d", path)
             .on('click', function(d,i,e) {
-                document.getElementById('tooltip-title').textContent = d.properties.ADMIN;
-                if(d.properties.ADMIN == 'Mexico') {
-                    document.getElementById('tooltip-text').textContent = `El Fondo TIBET todavía no tiene iniciativas en este país`;
-                } else {
-                    document.getElementById('tooltip-text').textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices, urna ac posuere tristique, mi urna convallis neque, vitae convallis velit enim sollicitudin dolor. Nunc sodales fringilla est non ultrices. Nulla ut sapien a augue aliquam semper. Proin sagittis congue posuere. Aliquam id libero pulvinar, laoreet lectus ac, euismod urna. Maecenas dictum sodales mauris, nec fringilla ex sagittis a. Cras id libero elementum, tempus eros molestie, sagittis augue. Nunc laoreet, sem vel blandit auctor, ipsum dolor feugiat nunc, nec elementum arcu sem vel sapien`;
+                let html = '<p class="chart__tooltip--title"> ' + d.properties.ADMIN + '</p>';
+                for(let i = 0; i < d.data.length; i++) {
+                    html += '<div class="chart__tooltip_b-text"><p class="chart__tooltip--text" id="tooltip-text">' + d.data[i].Titulo_ES + '</p><a href="https:www.elconfidencial.com" target=_blank>Consulta aquí</a></div>';
                 }
+                document.getElementById('tooltip-content').innerHTML = html;
+
                 tooltip.classed('visible', true);
             });
-    
+        
+        //Países sin datos
+        svg.selectAll("country")
+            .data(laWithout.features)
+            .enter()
+            .append("path")
+            .attr("class", "country-none")
+            .attr("fill", '#d9d9d9')
+            .attr("d", path);
+
+        //Todos
+        // svg.selectAll(".country")
+        //     .data(laGeojson.features)
+        //     .enter()
+        //     .append("path")
+        //     .attr("class", "country")
+        //     .attr("fill", 'red')
+        //     .attr("d", path);
+        
+        //Otros
         svg.append("path")
             .datum(worldGeojsonInner)
             .attr("fill", "none")
-            .attr("stroke", "#262626")
+            .attr("stroke", "#c3c3c3")
             .attr("d", path);
 
 
@@ -68,10 +113,4 @@ d3.queue()
 
             tooltip.classed('visible',false);
         });
-    })
-
-d3.json('', function(error,worldTopojson) {
-    if (error) throw error;
-
-    
-});
+    });
